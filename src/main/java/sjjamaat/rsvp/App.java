13afbd3801f6f8624f458743b3dcbbbba03973da
husaindalal <husaindalal@gmail.com>
2014-12-09 21:02:55 -0800

    ver1

---
+++ src/main/java/sjjamaat/rsvp/App.java
@@ -0,0 +1,13 @@
+package sjjamaat.rsvp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
